5d30ae63218f328e1f2df39e173cbc37457ff6f9
Tim Fischbach <tfischbach@codevise.de>
2017-11-02 16:26:28 +0100

    wip

--- script.rb
+++ script.rb
@@ -3,7 +3,6 @@ if ENV['ACTIVATE_BEFORE_SETUP'] == 'true'
   puts Gem.activate_bin_path('bundler', 'bundle', '>= 0.a')
 end
 
-require 'rubygems'
 require 'bundler/setup'
 
 puts "After bundler/setup"
