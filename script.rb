eb406b73e387371a2b92af63f32548f297d98f7d
Tim Fischbach <tfischbach@codevise.de>
2017-11-02 18:06:57 +0100

    wip

--- script.rb
+++ script.rb
@@ -5,7 +5,7 @@ end
 
 require 'bundler/setup'
 
-spec = p(Gem.send!(:find_spec_for_exe, 'bundler', 'bundle', ['=> 0.a']))
+spec = p(Gem.send(:find_spec_for_exe, 'bundler', 'bundle', ['=> 0.a']))
 puts spec.bin_file('bundle')
 
 puts "After bundler/setup"
