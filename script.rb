7e2ac70f1ff3203579782474433141e6dbcbbcd0
Tim Fischbach <tfischbach@codevise.de>
2017-11-02 16:29:08 +0100

    wip

--- script.rb
+++ script.rb
@@ -8,4 +8,4 @@ require 'bundler/setup'
 puts "After bundler/setup"
 puts Gem.activate_bin_path('bundler', 'bundle', '>= 0.a')
 
-system "bundle exec ruby -e 'puts 123'"
+fail unless system("bundle exec ruby -e 'puts 123'")
